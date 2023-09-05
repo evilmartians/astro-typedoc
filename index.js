71f7aa2ea455ce684453356c45c4d69a190699ac
Felix Khafizov <f_khafizov@protonmail.com>
2023-09-06 00:46:37 +0300

    Naive navigation realization

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-import { Application, TSConfigReader } from 'typedoc'
+import { writeFile } from 'node:fs/promises'
+import { Application, PageEvent, TSConfigReader } from 'typedoc'
+
+function onRendererPageEnd(event) {
+  if (!event.contents) {
+    return
+  } else if (/README\.md$/.test(event.url)) {
+    event.preventDefault()
+    return
+  }
+
+  let frontmatter = `---
+title: '${event.model.name}'
+layout: '../../../layouts/DocLayout.astro'
+---
+    `
+
+  event.contents = frontmatter + event.contents
+}
+
+function getNavigationFromProject(baseUrl = '', project) {
+  let baseUrlWithoutTrailingSlash = baseUrl.replace('/$', '')
+
+  let nav = project.groups
+    .map(group => {
+      return group.children.map(groupChild => {
+        return {
+          title: groupChild.name,
+          url: `${baseUrlWithoutTrailingSlash}/${groupChild.url}`.replace(
+            /\.md$/,
+            ''
+          )
+        }
+      })
+    })
+    .flat()
+
+  return nav
+}
 
 export async function generateApiDocs({
+  baseUrl = '/docs',
   entryPoints,
   pagesDirectory = 'src/pages/docs',
   tsconfig
@@ -8,6 +47,7 @@ export async function generateApiDocs({
   let app = new Application()
 
   app.options.addReader(new TSConfigReader())
+  app.renderer.on(PageEvent.END, event => onRendererPageEnd(event))
 
   await app.bootstrapWithPlugins({
     entryPoints,
@@ -28,4 +68,8 @@ export async function generateApiDocs({
   let project = await app.convert()
 
   await app.generateDocs(project, pagesDirectory)
+
+  let navigation = getNavigationFromProject(baseUrl, project)
+
+  await writeFile('src/nav.json', JSON.stringify(navigation))
 }
