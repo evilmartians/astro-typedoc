247a18002834c25180ca14f1862c42ddeb04e036
Felix Khafizov <f_khafizov@protonmail.com>
2023-09-20 19:39:45 +0300

    Add test environment for case when user reuses css only

---
+++ test/website-custom-layout/astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import initAstroTypedoc from '../../index.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const astroTypedoc = await initAstroTypedoc({
+  baseUrl: '/docs/',
+  entryPoints: [{ path: resolve(__dirname, '../../../nanostores/index.d.ts') }],
+  tsconfig: resolve(__dirname, '../../../nanostores/tsconfig.json')
+})
+
+const project = await astroTypedoc.getReflections()
+
+await astroTypedoc.generateDocs({
+  frontmatter: {
+    layout: resolve(__dirname, './src/components/DocLayout.astro')
+  },
+  outputFolder: 'src/pages/docs',
+  project
+})
+
+await astroTypedoc.generateNavigationJSON(project, resolve(__dirname, './src/'))
+
+// https://astro.build/config
+export default defineConfig({})
